95789de65b99355ae45522397d7ed7b170a16551
mrsatyams <satyams@codetta.digital>
2026-05-27 17:46:09 +0100

    update

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     // Component.Darkmode(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    Component.TableOfContents(),
     // Component.DesktopOnly(Component.Explorer()),
     // Component.DesktopOnly(Component.Graph()),
     // Component.DesktopOnly(Component.Backlinks()),
